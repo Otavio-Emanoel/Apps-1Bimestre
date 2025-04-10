61da3af8f53b538f03d0dfb7d3824dd8091d044c
Otavio Emanoel <otavioemanuel6b@gmail.com>
2025-04-10 14:26:28 -0300

    Add files via upload

---
+++ Apps-1Bimestre/AllForms.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Apps_1Bimestre
+{
+    partial class AllForms
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.app1Btn = new System.Windows.Forms.Button();
+            this.app2Btn = new System.Windows.Forms.Button();
+            this.app3Btn = new System.Windows.Forms.Button();
+            this.app4Btn = new System.Windows.Forms.Button();
+            this.app5Btn = new System.Windows.Forms.Button();
+            this.app6Btn = new System.Windows.Forms.Button();
+            this.app7Btn = new System.Windows.Forms.Button();
+            this.sairBtn = new System.Windows.Forms.Button();
+            this.pararBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // app1Btn
+            // 
+            this.app1Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app1Btn.Location = new System.Drawing.Point(12, 36);
+            this.app1Btn.Name = "app1Btn";
+            this.app1Btn.Size = new System.Drawing.Size(168, 60);
+            this.app1Btn.TabIndex = 0;
+            this.app1Btn.Text = "App 1";
+            this.app1Btn.UseVisualStyleBackColor = true;
+            // 
+            // app2Btn
+            // 
+            this.app2Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app2Btn.Location = new System.Drawing.Point(12, 146);
+            this.app2Btn.Name = "app2Btn";
+            this.app2Btn.Size = new System.Drawing.Size(168, 60);
+            this.app2Btn.TabIndex = 1;
+            this.app2Btn.Text = "App 2";
+            this.app2Btn.UseVisualStyleBackColor = true;
+            // 
+            // app3Btn
+            // 
+            this.app3Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app3Btn.Location = new System.Drawing.Point(12, 245);
+            this.app3Btn.Name = "app3Btn";
+            this.app3Btn.Size = new System.Drawing.Size(168, 60);
+            this.app3Btn.TabIndex = 2;
+            this.app3Btn.Text = "App 3";
+            this.app3Btn.UseVisualStyleBackColor = true;
+            // 
+            // app4Btn
+            // 
+            this.app4Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app4Btn.Location = new System.Drawing.Point(620, 36);
+            this.app4Btn.Name = "app4Btn";
+            this.app4Btn.Size = new System.Drawing.Size(168, 60);
+            this.app4Btn.TabIndex = 3;
+            this.app4Btn.Text = "App 4";
+            this.app4Btn.UseVisualStyleBackColor = true;
+            // 
+            // app5Btn
+            // 
+            this.app5Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app5Btn.Location = new System.Drawing.Point(620, 146);
+            this.app5Btn.Name = "app5Btn";
+            this.app5Btn.Size = new System.Drawing.Size(168, 60);
+            this.app5Btn.TabIndex = 4;
+            this.app5Btn.Text = "App 5";
+            this.app5Btn.UseVisualStyleBackColor = true;
+            // 
+            // app6Btn
+            // 
+            this.app6Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app6Btn.Location = new System.Drawing.Point(620, 245);
+            this.app6Btn.Name = "app6Btn";
+            this.app6Btn.Size = new System.Drawing.Size(168, 60);
+            this.app6Btn.TabIndex = 5;
+            this.app6Btn.Text = "App 6";
+            this.app6Btn.UseVisualStyleBackColor = true;
+            // 
+            // app7Btn
+            // 
+            this.app7Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app7Btn.Location = new System.Drawing.Point(325, 245);
+            this.app7Btn.Name = "app7Btn";
+            this.app7Btn.Size = new System.Drawing.Size(168, 60);
+            this.app7Btn.TabIndex = 6;
+            this.app7Btn.Text = "App 7";
+            this.app7Btn.UseVisualStyleBackColor = true;
+            // 
+            // sairBtn
+            // 
+            this.sairBtn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sairBtn.Location = new System.Drawing.Point(12, 381);
+            this.sairBtn.Name = "sairBtn";
+            this.sairBtn.Size = new System.Drawing.Size(134, 46);
+            this.sairBtn.TabIndex = 7;
+            this.sairBtn.Text = "Sair";
+            this.sairBtn.UseVisualStyleBackColor = true;
+            // 
+            // pararBtn
+            // 
+            this.pararBtn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pararBtn.Location = new System.Drawing.Point(654, 381);
+            this.pararBtn.Name = "pararBtn";
+            this.pararBtn.Size = new System.Drawing.Size(134, 46);
+            this.pararBtn.TabIndex = 8;
+            this.pararBtn.Text = "Parar App";
+            this.pararBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("News701 BT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 42);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Menu de Apps";
+            // 
+            // AllForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pararBtn);
+            this.Controls.Add(this.sairBtn);
+            this.Controls.Add(this.app7Btn);
+            this.Controls.Add(this.app6Btn);
+            this.Controls.Add(this.app5Btn);
+            this.Controls.Add(this.app4Btn);
+            this.Controls.Add(this.app3Btn);
+            this.Controls.Add(this.app2Btn);
+            this.Controls.Add(this.app1Btn);
+            this.Name = "AllForms";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button app1Btn;
+        private System.Windows.Forms.Button app2Btn;
+        private System.Windows.Forms.Button app3Btn;
+        private System.Windows.Forms.Button app4Btn;
+        private System.Windows.Forms.Button app5Btn;
+        private System.Windows.Forms.Button app6Btn;
+        private System.Windows.Forms.Button app7Btn;
+        private System.Windows.Forms.Button sairBtn;
+        private System.Windows.Forms.Button pararBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Apps-1Bimestre/AllForms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apps_1Bimestre
+{
+    public partial class AllForms : Form
+    {
+        public AllForms()
+        {
+            InitializeComponent();
+        }
+    }
+}
