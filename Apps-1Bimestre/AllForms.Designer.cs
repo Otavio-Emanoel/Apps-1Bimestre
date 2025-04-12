2435ab307a97d261102ea271b5a13704472de21f
Otavio Emanoel <otavioemanuel6b@gmail.com>
2025-04-12 11:22:30 -0300

    Conclusão do projeto

--- Apps-1Bimestre/AllForms.Designer.cs
+++ Apps-1Bimestre/AllForms.Designer.cs
@@ -42,101 +42,110 @@
             // 
             // app1Btn
             // 
-            this.app1Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app1Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app1Btn.Location = new System.Drawing.Point(12, 36);
             this.app1Btn.Name = "app1Btn";
             this.app1Btn.Size = new System.Drawing.Size(168, 60);
             this.app1Btn.TabIndex = 0;
             this.app1Btn.Text = "App 1";
             this.app1Btn.UseVisualStyleBackColor = true;
+            this.app1Btn.Click += new System.EventHandler(this.app1Btn_Click);
             // 
             // app2Btn
             // 
-            this.app2Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app2Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app2Btn.Location = new System.Drawing.Point(12, 146);
             this.app2Btn.Name = "app2Btn";
             this.app2Btn.Size = new System.Drawing.Size(168, 60);
             this.app2Btn.TabIndex = 1;
             this.app2Btn.Text = "App 2";
             this.app2Btn.UseVisualStyleBackColor = true;
+            this.app2Btn.Click += new System.EventHandler(this.app2Btn_Click);
             // 
             // app3Btn
             // 
-            this.app3Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app3Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app3Btn.Location = new System.Drawing.Point(12, 245);
             this.app3Btn.Name = "app3Btn";
             this.app3Btn.Size = new System.Drawing.Size(168, 60);
             this.app3Btn.TabIndex = 2;
             this.app3Btn.Text = "App 3";
             this.app3Btn.UseVisualStyleBackColor = true;
+            this.app3Btn.Click += new System.EventHandler(this.app3Btn_Click);
             // 
             // app4Btn
             // 
-            this.app4Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app4Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app4Btn.Location = new System.Drawing.Point(620, 36);
             this.app4Btn.Name = "app4Btn";
             this.app4Btn.Size = new System.Drawing.Size(168, 60);
             this.app4Btn.TabIndex = 3;
             this.app4Btn.Text = "App 4";
             this.app4Btn.UseVisualStyleBackColor = true;
+            this.app4Btn.Click += new System.EventHandler(this.app4Btn_Click);
             // 
             // app5Btn
             // 
-            this.app5Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app5Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app5Btn.Location = new System.Drawing.Point(620, 146);
             this.app5Btn.Name = "app5Btn";
             this.app5Btn.Size = new System.Drawing.Size(168, 60);
             this.app5Btn.TabIndex = 4;
             this.app5Btn.Text = "App 5";
             this.app5Btn.UseVisualStyleBackColor = true;
+            this.app5Btn.Click += new System.EventHandler(this.app5Btn_Click);
             // 
             // app6Btn
             // 
-            this.app6Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app6Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app6Btn.Location = new System.Drawing.Point(620, 245);
             this.app6Btn.Name = "app6Btn";
             this.app6Btn.Size = new System.Drawing.Size(168, 60);
             this.app6Btn.TabIndex = 5;
             this.app6Btn.Text = "App 6";
             this.app6Btn.UseVisualStyleBackColor = true;
+            this.app6Btn.Click += new System.EventHandler(this.app6Btn_Click);
             // 
             // app7Btn
             // 
-            this.app7Btn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app7Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.app7Btn.Location = new System.Drawing.Point(325, 245);
             this.app7Btn.Name = "app7Btn";
             this.app7Btn.Size = new System.Drawing.Size(168, 60);
             this.app7Btn.TabIndex = 6;
             this.app7Btn.Text = "App 7";
             this.app7Btn.UseVisualStyleBackColor = true;
+            this.app7Btn.Click += new System.EventHandler(this.app7Btn_Click);
             // 
             // sairBtn
             // 
-            this.sairBtn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sairBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.sairBtn.Location = new System.Drawing.Point(12, 381);
             this.sairBtn.Name = "sairBtn";
             this.sairBtn.Size = new System.Drawing.Size(134, 46);
             this.sairBtn.TabIndex = 7;
             this.sairBtn.Text = "Sair";
             this.sairBtn.UseVisualStyleBackColor = true;
+            this.sairBtn.Click += new System.EventHandler(this.sairBtn_Click);
             // 
             // pararBtn
             // 
-            this.pararBtn.Font = new System.Drawing.Font("News701 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pararBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pararBtn.Location = new System.Drawing.Point(654, 381);
             this.pararBtn.Name = "pararBtn";
             this.pararBtn.Size = new System.Drawing.Size(134, 46);
             this.pararBtn.TabIndex = 8;
             this.pararBtn.Text = "Parar App";
             this.pararBtn.UseVisualStyleBackColor = true;
+            this.pararBtn.Click += new System.EventHandler(this.pararBtn_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("News701 BT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(270, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 42);
+            this.label1.Size = new System.Drawing.Size(250, 39);
             this.label1.TabIndex = 9;
             this.label1.Text = "Menu de Apps";
             // 
--- Apps-1Bimestre/AllForms.cs
+++ Apps-1Bimestre/AllForms.cs
@@ -16,5 +16,320 @@ namespace Apps_1Bimestre
         {
             InitializeComponent();
         }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            MessageBox.Show("O fechamento foi desabilitado. Você deve fechar o aplicativo de outra maneira.");
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ClassStyle |= 0x200;
+                return cp;
+            }
+        }
+
+        private void app1Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("WinFormsPesoIdeal").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App1\\WinFormsPesoIdeal.exe");
+                }
+
+                if (System.Diagnostics.Process.GetProcessesByName("WinFormsPesoIdeal").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App1 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void app2Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("App2CalcularMediaAluno").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App2\\App2CalcularMediaAluno.exe");
+                }
+                if (System.Diagnostics.Process.GetProcessesByName("App2CalcularMediaAluno").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App2 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void sairBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void app3Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("App3-EPI-Funcionario").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App3\\App3-EPI-Funcionario.exe");
+                }
+                if (System.Diagnostics.Process.GetProcessesByName("App3-EPI-Funcionario").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App3 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void app4Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("App4-Habilitacao").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App4\\App4-Habilitacao.exe");
+                }
+                if (System.Diagnostics.Process.GetProcessesByName("App4-Habilitacao").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App4 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void app5Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("App5-Triangulos").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App5\\App5-Triangulos.exe");
+                }
+                if (System.Diagnostics.Process.GetProcessesByName("App5-Triangulos").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App5 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void app6Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("App6_Sistema_Vendas").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App6\\App6_Sistema_Vendas.exe");
+                }
+                if (System.Diagnostics.Process.GetProcessesByName("App6_Sistema_Vendas").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App6 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void app7Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Diagnostics.Process.GetProcessesByName("App7-Advinhacoes").Length == 0)
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\otavi\\Source\\Repos\\Apps-1Bimestre\\Apps-1Bimestre\\Apps\\App7\\App7-Advinhacoes.exe");
+                }
+                if (System.Diagnostics.Process.GetProcessesByName("App7-Advinhacoes").Length != 0)
+                {
+                    app1Btn.Enabled = false;
+                    app2Btn.Enabled = false;
+                    app3Btn.Enabled = false;
+                    app4Btn.Enabled = false;
+                    app5Btn.Enabled = false;
+                    app6Btn.Enabled = false;
+                    app7Btn.Enabled = false;
+                    MessageBox.Show("O App7 está em execução!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void pararBtn_Click(object sender, EventArgs e)
+        {
+            if (System.Diagnostics.Process.GetProcessesByName("WinFormsPesoIdeal").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("WinFormsPesoIdeal");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+            if (System.Diagnostics.Process.GetProcessesByName("App2CalcularMediaAluno").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("App2CalcularMediaAluno");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+            if (System.Diagnostics.Process.GetProcessesByName("App3-EPI-Funcionario").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("App3-EPI-Funcionario");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+            if (System.Diagnostics.Process.GetProcessesByName("App4-Habilitacao").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("App4-Habilitacao");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+            if (System.Diagnostics.Process.GetProcessesByName("App5-Triangulos").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("App5-Triangulos");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+            if (System.Diagnostics.Process.GetProcessesByName("App6_Sistema_Vendas").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("App6_Sistema_Vendas");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+            if (System.Diagnostics.Process.GetProcessesByName("App7-Advinhacoes").Length != 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("App7-Advinhacoes");
+                foreach (System.Diagnostics.Process process in processes)
+                {
+                    process.Kill();
+                    app1Btn.Enabled = true;
+                    app2Btn.Enabled = true;
+                    app3Btn.Enabled = true;
+                    app4Btn.Enabled = true;
+                    app5Btn.Enabled = true;
+                    app6Btn.Enabled = true;
+                    app7Btn.Enabled = true;
+                }
+            }
+
+        }
+
     }
 }
